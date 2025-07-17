f6b0f6da934e644d0c88bec1721c3e5435519755
NMD <minhducnguyenit96@gmail.com>
2025-07-17 15:12:59 +0700

    v10

--- practices.sh
+++ practices.sh
@@ -116,6 +116,40 @@ Practices06(){
 	ls -lna  $1
 }
 
+Practices07(){
+	echo "list all file in a folder, the folder path come from the keyboard, list the file is can wrx"
+	echo ""
+	echo "folder path is: $1"
+	current_working_dict=$(pwd)
+
+	echo "accessing the folder: $1"
+	cd $1
+	$(pwd)
+
+	files=$(ls -lna $1)
+
+	for file in $files
+	do
+		if [ -f $file -a -s $file ]; then 
+			echo "INFO:: test file: $file"
+			if [ -r $1 ]; then 
+				echo "file with enable -r"
+			fi 
+			if [ -x $1 ]; then 
+				echo "file with enable -x"
+			fi 
+			if [ -w $1 ]; then 
+				echo "file with enable -w"
+			fi 
+		# else
+		#     echo "the item $file not match condition is file or non empty"
+		fi 
+	done 
+
+	echo "Completed task. exit folder"
+
+	cd ${current_working_dict}
+}
 
 
 while true;
@@ -154,6 +188,11 @@ case $choice in
 
 	6)
 	Practices06
+	pause_screen
+	;;
+
+	7)
+	Practices07
 	pause_screen
 	;;
 
