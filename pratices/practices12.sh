95726c41841fdf2a20e72bc6f7f6cb3403c69ee4
Nguyen Minh Duc <minhducnguyenit96@gmail.com>
2025-07-17 22:11:19 +0700

    v24

---
+++ pratices/practices12.sh
@@ -0,0 +1,15 @@
+if [$# -eq 0];
+then
+echo "bạn phải điền: $0 <Thư mục>"
+exit 1
+fi
+
+if [! -d "$1"];
+then
+echo "Thư mục không tồn tại"
+exit 1
+fi
+
+echo "Nội dung thư mục: $1"
+echo ""
+ls -la "$1" 
