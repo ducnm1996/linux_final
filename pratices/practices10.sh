6b092af528eb9fe0d73823cc50f6330b0ba12afc
Nguyen Minh Duc <minhducnguyenit96@gmail.com>
2025-07-17 21:32:13 +0700

    v20

--- pratices/practices10.sh
+++ pratices/practices10.sh
@@ -1,9 +1,3 @@
-if [$# -eq 0];
-then
-echo "Bạn cần nhập: \$lunar_year <năm>"
-exit 1
-fi
-
 year=$1
 can=("Giáp" "Ất" "Bính" "Đinh" "Mậu" "Kỷ" "Canh" "Tân" "Nhâm" "Quý")
 chi=("Tý" "Sửu" "Dần" "Mão" "Thìn" "Tị" "Ngọ" "Mùi" "Thân" "Dậu" "Tuất" "Hợi")
